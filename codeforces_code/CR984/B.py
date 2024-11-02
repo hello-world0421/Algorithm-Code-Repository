77b94ccf179d5ce70c41507068f381bd910451ea
moyu <2447656632@qq.com>
2024-11-03 00:24:51 +0800

    fix some bug about redefined-builtin

--- codeforces_code/CR984/B.py
+++ codeforces_code/CR984/B.py
@@ -1,11 +1,11 @@
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    dict = {}
+    dic = {}
     for _ in range(k):
         brand, cost = map(int, input().split())
-        dict[brand] = dict.get(brand, 0) + cost
+        dic[brand] = dic.get(brand, 0) + cost
     ans = 0
-    for [brand, cost] in sorted(dict.items(), key=lambda a: -a[1]):
+    for [brand, cost] in sorted(dic.items(), key=lambda a: -a[1]):
         if n == 0:
             break
         ans += cost
